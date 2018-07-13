97a9d006bce9afa154130a0e45b15aa9daee1d59
Lauren Meier <meier.lauren@gmail.com>
2018-07-13 14:34:58 -0700

    add new home page template and create links to different post lists

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,9 @@ from .forms import PostForm
 
 # Create your views here.
 
+def home_page(request):
+	return render(request, 'blog/home_page.html')
+
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
